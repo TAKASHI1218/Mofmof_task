640e455c97d8b6463b2881e9ef0246c0eeedfaaa
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-10-16 08:02:47 +0000

    Created model which name is building and controller

---
+++ db/migrate/20191016075921_create_buildings.rb
@@ -0,0 +1,13 @@
+class CreateBuildings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :buildings do |t|
+      t.text :name
+      t.integer :rent
+      t.text :address
+      t.integer :age
+      t.text :info
+
+      t.timestamps
+    end
+  end
+end
